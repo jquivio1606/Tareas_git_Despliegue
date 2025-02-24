ed7deea44522f291677674054c03bfbeedaa80b9
Judit <jquicio1606@g.educaand.es>
2025-02-24 18:27:47 +0100

    Subir cosas al github

---
+++ Calculadora.java
@@ -0,0 +1,26 @@
+/**
+* Clase para demostrar el uso de Javadoc.
+* Esta clase contiene dos métodos simples: uno para sumar y otro para restar dos números.
+*/
+public class Calculadora {
+    /**
+    * Suma dos números enteros.
+    *
+    * @param a El primer número a sumar.
+    * @param b El segundo número a sumar.
+    * @return El resultado de la suma de a y b.
+    */
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+    /**
+    * Resta dos números enteros.
+    *
+    * @param a El número del cual se va a restar.
+    * @param b El número que se va a restar.
+    * @return El resultado de restar b a a.
+    */
+    public int restar(int a, int b) {
+        return a - b;
+    }
+}
