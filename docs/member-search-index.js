2043a5d821be1c3420c11a5be2e19fd498e72d6d
Judit <jquivio1606@g.educaand.es>
2025-03-01 20:09:49 +0100

    Subir documentacion

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Calculadora","l":"Calculadora()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Calculadora","l":"restar(int, int)","u":"restar(int,int)"},{"p":"<Unnamed>","c":"Calculadora","l":"sumar(int, int)","u":"sumar(int,int)"}];updateSearchResults();
